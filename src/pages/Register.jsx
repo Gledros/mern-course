b59c36e04b4e3d4f9bb4c238a5a740ae2e9b0d78
Glenn Della Rocca <glenn@dellarocca.mx>
2022-06-24 12:09:28 -0600

    Extend Register page
    Modified Register page to include it's first form with 3 basic fields.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,35 @@
-import Wrapper from '@wrappers/LandingPage'
+import Wrapper from '@wrappers/RegisterPage'
+import { useState, useEffect } from 'react'
+import { Logo, FormRow } from '@components'
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+}
 
 const Register = () => {
-  return <Wrapper>
-    <h1>Register</h1>
+  const [values, setValues] = useState(initialState)
+
+  const handleChange = e => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+
+  return <Wrapper className='full-page'>
+    <form className='form' onSubmit={onSubmit}>
+      <Logo />
+      <h3>Register</h3>
+      <FormRow type='text' name='name' value={values.name} handleChange={handleChange} />
+      <FormRow type='email' name='email' value={values.email} handleChange={handleChange} />
+      <FormRow type='password' name='password' value={values.password} handleChange={handleChange} />
+      <button type='submit' className='btn btn-block'>register</button>
+    </form>
   </Wrapper>
 }
 

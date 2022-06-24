a3e9a4bc80e2820cf8ef6ed04b8823b8cbdf8da5
Glenn Della Rocca <glenn@dellarocca.mx>
2022-06-24 10:41:44 -0600

    Modify the error page
    It makes the error page more appealing and informative.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom'
+import img from '@images/not-found.svg'
+import Wrapper from '@wrappers/ErrorPage'
 
 const Error = () => {
-  return (
-    <h4>Error</h4>
-  )
+  return <Wrapper>
+    <div>
+      <img src={img} alt="not found" />
+      <h3>Wops! page not found</h3>
+      <p>We can't seem to find the page you're looking for</p>
+      <Link to='/'>back home</Link>
+    </div>
+  </Wrapper>
 }
 export default Error

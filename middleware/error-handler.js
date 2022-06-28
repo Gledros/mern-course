8c72d24eb7c6770b2537e94df3202af3ea3c5dc9
Glenn Della Rocca <glenn@dellarocca.mx>
2022-06-28 16:43:54 -0600

    Create error handler middleware

---
+++ middleware/error-handler.js
@@ -0,0 +1,7 @@
+
+const errorHandlerMiddleware = (error, request, response, next) => {
+  console.log(error)
+  response.status(500).json({ message: 'Something went wrong' })
+}
+
+export default errorHandlerMiddleware

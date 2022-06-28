d2bda1880d336ad0109fa71d1ab02eefaf95bd79
Glenn Della Rocca <glenn@dellarocca.mx>
2022-06-28 16:44:15 -0600

    Implement error handler middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
 dotenv.config()
 
@@ -8,9 +9,10 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.get('/', (request, response) => {
-  response.send('Hello World!')
+  throw new Error('This is a test error')
 })
 
+app.use(errorHandlerMiddleware)
 app.use(notFoundMiddleware)
 
 app.listen(PORT, () => {

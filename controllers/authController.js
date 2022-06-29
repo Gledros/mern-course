302527b0d10e7f9f5139e874aadc182c9fb52950
Glenn Della Rocca <glenn@dellarocca.mx>
2022-06-29 10:27:24 -0600

    Create & implement authController skeleton
    Added also express.json middleware to access JSON data posted.

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+
+const register = (request, response) => {
+  response.send('register user')
+}
+
+const login = (request, response) => {
+  response.send('login user')
+}
+
+const updateUser = (request, response) => {
+  response.send('update user')
+}
+
+export { register, login, updateUser }
--- server.js
+++ server.js
@@ -4,16 +4,23 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
+// DB & authentication
 import connectDB from './db/connect.js'
 
+// Routers
+import router from './routes/authRoutes.js'
 
 // Middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
   response.send('Hello World')
 })
+
+app.use('/api/v1/auth', router)
 
 app.use(errorHandlerMiddleware)
 app.use(notFoundMiddleware)

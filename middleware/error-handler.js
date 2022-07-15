b05709f6b1051fb319d1b70eef299fd1779dc541
Glenn Della Rocca <glenn@dellarocca.mx>
2022-07-15 15:19:01 -0600

    Implement errorHandleMiddleware custom error data

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,32 @@
+import { StatusCodes as HTTP } from "http-status-codes";
+
 const errorHandlerMiddleware = (error, request, response, next) => {
   console.log(error);
-  response.status(500).json({ message: error });
+  let errorData;
+
+  if (error.code && error.code === 11000) {
+    errorData = {
+      code: HTTP.BAD_REQUEST,
+      message: `${Object.keys(error.keyValue)} field has to be unique`
+    };
+  } else {
+    const defaultError = {
+      code: HTTP.INTERNAL_SERVER_ERROR,
+      message: "Something went wrong, try again later"
+    };
+
+    errorData =
+      {
+        ValidationError: {
+          code: HTTP.BAD_REQUEST,
+          message: Object.values(error.errors)
+            .map(currentError => currentError.message)
+            .join(". ")
+        }
+      }[error.name] || defaultError;
+  }
+
+  response.status(errorData.code).json({ message: errorData.message });
 };
 
 export default errorHandlerMiddleware;

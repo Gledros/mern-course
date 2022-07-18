19ee23ea5f656c9fb121ca6c327aad14bff4c3de
Glenn Della Rocca <glenn@dellarocca.mx>
2022-07-18 10:51:15 -0600

    Create CustomAPIError class

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,21 @@
 import User from "../models/User.js";
 import { StatusCodes as HTTP } from "http-status-codes";
 
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+    this.code = HTTP.BAD_REQUEST;
+    this.errors = [];
+  }
+}
+
 const register = async (request, response) => {
+  const { name, email, password } = request.body;
+
+  if (!name || !email || !password) {
+    throw new CustomAPIError("Please provide all the values");
+  }
+
   const user = await User.create(request.body);
   response.status(HTTP.OK).json({ user });
 };

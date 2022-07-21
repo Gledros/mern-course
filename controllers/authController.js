eca6193c77f8f4a9934152b2220ee547b96c6b63
Glenn Della Rocca <glenn@dellarocca.mx>
2022-07-21 11:58:20 -0600

    Fix authController register endpoint response

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,8 +16,18 @@ const register = async (request, response) => {
     throw new CustomAPIError("Please provide all the values");
   }
 
-  const user = await User.create(request.body);
-  response.status(HTTP.OK).json({ user });
+  let user = await User.create(request.body);
+  const token = user.createJWT();
+
+  response.status(HTTP.OK).json({
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      location: user.location
+    },
+    token
+  });
 };
 
 const login = (request, response) => {

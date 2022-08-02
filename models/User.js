88d98aadb7e5bbbb6d1e6475286c54bb10b1b988
Glenn Della Rocca <glenn@dellarocca.mx>
2022-08-02 10:05:34 -0600

    Add password checking method

--- models/User.js
+++ models/User.js
@@ -1,53 +1,58 @@
-import mongoose from "mongoose";
-import validator from "validator";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import mongoose from 'mongoose'
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please provide name"],
+    required: [true, 'Please provide name'],
     minLength: 3,
     maxLength: 20,
     trim: true
   },
   email: {
     type: String,
-    required: [true, "Please provide email"],
+    required: [true, 'Please provide email'],
     unique: true,
     validate: {
       validator: validator.isEmail,
-      message: "Please provide a valid email"
+      message: 'Please provide a valid email'
     }
   },
   password: {
     type: String,
-    required: [true, "Please provide password"],
+    required: [true, 'Please provide password'],
     minLength: 6
   },
   lastName: {
     type: String,
     trim: true,
     maxLength: 20,
-    default: "Smith"
+    default: 'Smith'
   },
   location: {
     type: String,
     trim: true,
     maxLength: 20,
-    default: "my city"
+    default: 'my city'
   }
-});
+})
 
-UserSchema.pre("save", async function () {
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 UserSchema.methods.createJWT = function () {
   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRY
-  });
-};
+  })
+}
 
-export default mongoose.model("User", UserSchema);
+UserSchema.methods.comparePassword = async function (password) {
+  const isMatch = await bcrypt.compare(password, this.password)
+  return isMatch
+}
+
+export default mongoose.model('User', UserSchema)

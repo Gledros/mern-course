d2e664179260260c4d92750ae0427c0cba94c4a5
Glenn Della Rocca <glenn@dellarocca.mx>
2022-08-04 16:35:03 -0600

    Implement new protected dashboard pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,35 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import { Dashboard, Landing, Register, Error } from '@pages'
+import { Landing, Register, Error, ProtectedRoute } from '@pages'
+import { Stats, Jobs, AddJob, Profile, DashboardLayout } from '@pages/dashboard'
 
 const App = () => {
-  return <BrowserRouter>
-    <nav>
-      <Link to='/'>Dashboard</Link>
-      <Link to='/register'>Register</Link>
-      <Link to='/landing'>Landing</Link>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>Dashboard</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/landing'>Landing</Link>
+      </nav>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path='stats' element={<Stats />} />
+          <Route path='jobs' element={<Jobs />} />
+          <Route path='jobs/add' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
